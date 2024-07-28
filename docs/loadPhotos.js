5d614e029309acb4449ac1c9bd5b70352db495b5
Misha <banderwolfe06@gmail.com>
2024-07-28 09:40:34 +0200

    Update HTML and fix image paths

--- docs/loadPhotos.js
+++ docs/loadPhotos.js
@@ -1,10 +1,17 @@
-function loadPhotos(url, containerId) {
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById(containerId).innerHTML = data;
-        })
-        .catch(error => {
-            console.error('Error loading photos:', error);
-        });
+function togglePhotos(url, containerId, button) {
+    const container = document.getElementById(containerId);
+    if (container.innerHTML === "") {
+        fetch(url)
+            .then(response => response.text())
+            .then(data => {
+                container.innerHTML = data;
+                button.textContent = "Згорнути фото";
+            })
+            .catch(error => {
+                console.error('Error loading photos:', error);
+            });
+    } else {
+        container.innerHTML = "";
+        button.textContent = "Розгорнути фото";
+    }
 }
