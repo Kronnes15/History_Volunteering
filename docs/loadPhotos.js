519872e8b2891bb4d34cf277180943acfec39528
Misha <banderwolfe06@gmail.com>
2024-07-28 09:29:54 +0200

    Update HTML and fix image paths

---
+++ docs/loadPhotos.js
@@ -0,0 +1,10 @@
+function loadPhotos(url, containerId) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(containerId).innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading photos:', error);
+        });
+}
